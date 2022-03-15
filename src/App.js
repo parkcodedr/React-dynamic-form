2218e8cfbdcf9f17056747b7fd876cb2061e9a41
parkcodedr <okwori32@gmail.com>
2022-03-15 12:26:56 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -35,29 +35,47 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={submit}>
-        {formFields.map((form, index) => {
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-8">
+            {JSON.stringify(formFields)}
+          {formFields.map((form, index) => {
           return (
-            <div key={index}>
+              <div className="row" key={index}>
+           
+              <div className="col">
               <input
                 name='name'
+                className="form-control mb-2"
                 placeholder='Name'
                 onChange={event => handleFormChange(event, index)}
                 value={form.name}
               />
+              </div>
+              <div className="col">
               <input
                 name='age'
                 placeholder='Age'
+                className="form-control mb-2"
                 onChange={event => handleFormChange(event, index)}
                 value={form.age}
               />
-              <button onClick={() => removeFields(index)}>Remove</button>
+                </div>
+             <div className="col-1">
+             <button className="btn btn-danger" onClick={() => removeFields(index)}>Remove</button>
+             </div>
             </div>
+           
           )
         })}
+          </div>
+        </div>
+       
       </form>
-      <button onClick={addFields}>Add More..</button>
-      <br />
-      <button onClick={submit}>Submit</button>
+      <div className="row justify-content-center">
+      <button className="btn btn-success col-2" onClick={addFields}>Add More..</button>
+      <p></p>
+      <button className="btn btn-primary col-2 " onClick={submit}>Submit</button>
+      </div>
     </div>
   );
 }
